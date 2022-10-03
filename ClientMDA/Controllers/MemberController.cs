6f8b3f549b802e2f92f361e824d287a19bcc98ec
洪暐婷 <jo3wait@gmail.com>
2022-10-03 18:51:58 +0800

    片單、登入、基本資料

--- ClientMDA/Controllers/MemberController.cs
+++ ClientMDA/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClientMDA.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,63 @@ namespace ClientMDA.Controllers
 {
     public class MemberController : Controller
     {
-        public IActionResult Index()
+        List<CMemberDemoViewModel> members = new List<CMemberDemoViewModel>();
+        CMemberDemoViewModel memberDemo2 = new CMemberDemoViewModel
+        {
+            MemberID = 2,
+            Cellphone = "0902002002",
+            Email = "002@gmail.com",
+            Password = "0000",
+            Formal = false,
+            Permission = 0,
+            CreatedTime = DateTime.Now,
+        };
+        CMemberDemoViewModel memberDemo1 = new CMemberDemoViewModel
+        {
+            MemberID = 1,
+            Cellphone = "0901001001",
+            Email = "001@gmail.com",
+            Password = "0000",
+            Formal = true,
+            Permission = 0,
+            Address = "台北市",
+            BirthDate = new DateTime(1990, 1, 1),
+            F_Name = "Wei",
+            L_Name = "Wang",
+            NickName = "Yuka",
+            Gender = 1,
+            Bonus = 100,
+            CreatedTime = new DateTime(2022, 6, 15),
+        };
+        public IActionResult Login()
+        {            
+            return View();
+        }
+        public IActionResult Login2()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult MemberEdit()
+        {
+            return View();
+        }
+        public IActionResult CommentList()
+        {
+            return View();
+        }
+        public IActionResult CommentEdit()
+        {
+            return View();
+        }
+        public IActionResult FollowList()
+        {
+            return View();
+        }
+        public IActionResult WatchList()
         {
             return View();
         }
