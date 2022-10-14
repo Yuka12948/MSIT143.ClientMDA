b57261899cdc96a686e3975cbd21464baa02aa31
Yuka12948 <yuka12948@gmail.com>
2022-10-14 17:58:28 +0800

    排行樣式調整

--- ClientMDA/Controllers/MovieController.cs
+++ ClientMDA/Controllers/MovieController.cs
@@ -8,12 +8,12 @@ namespace ClientMDA.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult 電影排行()
+        public IActionResult 電影排行TOP5()
         {
             return View();
         }
 
-        public IActionResult 電影排行2()
+        public IActionResult 電影排行()
         {
             return View();
         }
